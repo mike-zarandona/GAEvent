479a296fe5fa703a0c91c055ee098c96ce2c7235
Mike Zarandona <mike.zarandona@gmail.com>
2014-03-18 10:50:25 -0400

    v1.0.5 - Added support for custom event types.

--- src/jquery.gaEvent.js
+++ src/jquery.gaEvent.js
@@ -2,26 +2,28 @@
  * jQuery.gaEvent
  * Description:		gaEvent makes it easy to write one-liner Google Event custom events.
  * Author:			Mike Zarandona
- * Version:			1.0.4
- *					Added support for classic and universal GA scripts
+ * Version:			1.0.5
+ *					Added support for passing in an event type
  *
- * Variables:		category [REQUIRED] [string] the name supplied for the group of objects to track
- * 					action   [REQUIRED] [string] a unique user-action paired with each category, and commonly used to define the type of user interaction for the web object
- * 					label    [OPTIONAL] [string] optional field to provide additional dimension to the event data
+ * Variables:		eventType [REQUIRED] [string] the Javascript event(s) on which to fire
+ *					category  [REQUIRED] [string] the name supplied for the group of objects to track
+ * 					action    [REQUIRED] [string] a unique user-action paired with each category, and commonly used to define the type of user interaction for the web object (this will usually be the same as the 'action')
+ * 					label     [OPTIONAL] [string] optional field to provide additional dimension to the event data
  *										pageURL   | label = the current page's URL
  * 										pageTitle | label = the current page's title
  *										linkDest  | label = the referenced link's destination (reference an a tag)
  *										linkTitle | label = the referenced link's title (reference an a tag)
  * 					value    [OPTIONAL] [int] integer that can be used to provide numerical data about the user event
  *
- * Usage:			$('#logo img').gaEvent('Logo', 'Click', 'Home');
- *					$('a#atag').gaEvent('Header Link', 'Click', 'pageTitle');
+ * Usage:			$('#logo img').gaEvent('click', 'Logo', 'Click', 'Home');
+ *					$('a#atag').gaEvent('click', 'Header Link', 'Click', 'pageTitle');
+ *					$('.some-thing').gaEvent('hover', 'Some Thing', 'Hover', 'pageURL');
  * 
  * Note: 			I STRONGLY recommend using the Google Analytics Debugger extension for Chrome (http://tinyurl.com/knv6kos) when implementing this plugin
 *********************************/
 
 (function($){
-	jQuery.fn.gaEvent = function(category, action, label, value) {
+	jQuery.fn.gaEvent = function(eventType, category, action, label, value) {
 
 		return this.each(function(){
 
@@ -52,14 +54,14 @@
 
 			// Universal (analytics.js | new)
 			if (typeof ga === 'function') {
-				$(this).on('click', function() {
+				$(this).on(eventType, function() {
 					ga('send', 'event', category, action, label, value);
 				});
 			}
 
 			// Classic (ga.js | old)
 			if (typeof _gaq !== 'undefined' && typeof _gaq.push === 'function') {
-				$(this).on('click', function() {
+				$(this).on(eventType, function() {
 					_gaq.push(['_trackEvent', category, action, label, value]);
 				});
 			}
